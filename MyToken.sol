fe4bd6fc00cceb159f6d37b9d801817f96ed3916
Ghazal E Ashar <145915937+ghazal-ashar@users.noreply.github.com>
2025-03-13 00:15:50 +0500

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Ghazal is ERC20, ERC20Permit {
-    constructor() ERC20("Ghazal", "GHZ") ERC20Permit("Ghazal") {}
+contract Ghazal is ERC20 {
+    constructor(address recipient) ERC20("Ghazal", "GHZ") {
+        _mint(recipient, 100000 * 10 ** decimals());
+    }
 }
