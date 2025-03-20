109891191268ae5bfbf758046736cd1483c2ea4f
Ghazal E Ashar <145915937+ghazal-ashar@users.noreply.github.com>
2025-03-21 00:48:14 +0500

    Update Lec5_flattened.sol with unstake

--- Lec5_flattened.sol
+++ Lec5_flattened.sol
@@ -669,18 +669,37 @@ contract Ghazal is ERC20 {
     }
 }
 
-contract Staking{
+contract Staking {
 
     Ghazal public token;
 
     mapping(address => uint256) public stakes;
-    uint8 constant public reward = 5;
+    uint8 public constant reward = 5;
 
-    function stake(uint256 amount) public{
+    constructor(address token_address) {
+        token = Ghazal(token_address);
+    }
+
+    function stake(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
 
         token.transfer(address(this), amount);
-        stakes[msg.sender] += amount;
 
+        stakes[msg.sender] += amount;
+    }
+
+    function unstake() external {
+       
+        uint256 stakedAmount = stakes[msg.sender];
+        require(stakedAmount > 0, "No tokens staked");
+
+        uint256 rewardAmount = (stakedAmount * reward) / 100;
+
+        uint256 amount = stakedAmount + rewardAmount;
+
+        token.transfer(msg.sender, amount);
+
+        stakes[msg.sender] = 0;
     }
 
 }
