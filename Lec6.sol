0d6455b5a21d0fb041a73ddf6aa0aa88f3f8d05a
Ghazal E Ashar <145915937+ghazal-ashar@users.noreply.github.com>
2025-03-27 11:17:02 +0500

    Create Lec6.sol

---
+++ Lec6.sol
@@ -0,0 +1,43 @@
+contract Staking {
+
+    Ghazal public token;
+    address public contractAdd;
+
+    mapping(address => uint256) public stakes;
+    uint256 public reward;
+
+    mapping(address => uint256) public stakeTime;
+
+    constructor(address token_address) {
+        token = Ghazal(token_address);
+        
+    }
+
+    function stake(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+
+        token.transferFrom(msg.sender,address(this), amount);
+
+        stakes[msg.sender] += amount;
+        stakeTime[msg.sender] = block.timestamp;
+    }
+
+    function unstake() external {
+       
+        uint256 stakedAmount = stakes[msg.sender];
+        uint256 time = stakeTime[msg.sender];
+        // require(block.timestamp >= (time + (1 days)), "Wait a minimum of 24 hours before unstaking.");
+        require(stakedAmount > 0, "No tokens staked");
+
+        reward = (block.timestamp - time) / 1 seconds;
+
+        uint256 rewardAmount = (stakedAmount * reward) / 100;
+
+        uint256 amount = stakedAmount + rewardAmount;
+
+        token.transferFrom(address(this), msg.sender, amount);
+
+        stakes[msg.sender] = 0;
+    }
+
+}
